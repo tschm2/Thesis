d3fa708a90ec6367d4ec85c475adc70ceca32a17
Guillaume Fricker <fricker.g@zoho.com>
2018-04-26 17:39:04 +0200

    Test deploy for AIML
    //FIXME: AIML File broken and probably too long

--- eMMA/aiml/test.js
+++ eMMA/aiml/test.js
@@ -3,18 +3,11 @@ var interpreter = new aimlHigh({name:'eMMA', age:'22'});
 interpreter.loadFiles(['german-1.aiml']);
 
 var callback = function(answer, wildCardArray, input){
-    console.log(input + ' | ' + answer);
-	console.log(wildCardArray);
-	console.log('------------------------');
+    console.log(answer + ' | ' + wildCardArray + ' | ' + input);
 };
 
-function normaliseString(str) {
-	temp = str.toUpperCase();
-	return temp.replace(/[^a-zA-Z ]/g, "");
-}
-
-var question = 'Ich mag Linux!';
-interpreter.findAnswer(normaliseString(question), callback);
-interpreter.findAnswer('Mein Name ist Guillaume.', callback);
-interpreter.findAnswer('Was ist mein Name?', callback);
-interpreter.findAnswer('Wie heisst du?', callback);
+interpreter.findAnswer('Ich mag Linux', callback);
+interpreter.findAnswer('Ich heisse Guillaume.', callback);
+interpreter.findAnswer('Wie heisse ich?', callback);
+interpreter.findAnswer('Gute Nacht, eMMA!', callback);
+interpreter.findAnswer('Absolut', callback);

dc1e0745540d88bef1fcda6ea5b90c3444999ee4
Guillaume Fricker <fricker.g@zoho.com>
2018-04-30 19:04:19 +0200

    Update dependencies and configurations and crossing fingers
    Doesn't work yet, but the build for android is finally successful!

--- eMMA/src/services/botService.ts
+++ eMMA/src/services/botService.ts
@@ -1,16 +1,19 @@
+import { Http, Response } from '@angular/http';
 import RiveScript from 'rivescript';
 
 export class BotService{
 	opts: any;
 	bot: RiveScript;
+	http: Http;
+
 	constructor() { }
 
 	init(){
 		this.opts = {
-			debug: false,
-			utf8: false,
+			debug: true,
+			utf8: true,
 			watch: false,
-			brain: './brain/german-1.rive'
+			brain: this.getData
 		};
 		this.bot = new RiveScript({
 			debug: this.opts.debug,
@@ -20,6 +23,11 @@ export class BotService{
 		(this.bot as any).ready = false;
 		console.log('loading bot');
 		this.bot.loadFile(this.opts.brain, this.loadingDone, this.loadingError);
+	}
+
+	getData() {
+		return this.http.get('./assets/brain/german.rive')
+			.subscribe((res: Response) => res.text());
 	}
 
 	loadingDone(batchNumber) {
@@ -37,4 +45,5 @@ export class BotService{
 			? this.bot.reply("localuser", request)
 			: "ERR: Bot Not Ready Yet";
 	}
+
 }

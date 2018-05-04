1ad89e7c483f58dcb6349690cbff8c53b5964341
Guillaume Fricker <fricker.g@zoho.com>
2018-05-04 13:59:16 +0200

    Using Promises to initialize bot
    //FIXME: bot is undefined in other methods (loadingDone) and only the programming gods know why

--- eMMA/src/services/botService.ts
+++ eMMA/src/services/botService.ts
@@ -4,58 +4,49 @@ import RiveScript from 'rivescript';
 
 @Injectable()
 export class BotService{
-	opts: any;
 	bot: RiveScript;
+	opts: any;
+	ready: boolean;
 	http: Http;
 
 	constructor(private h: Http) {
 		this.http = h;
-	}
+		this.ready = false;
 
-	init(){
-		let url = this.getData();
 		this.opts = {
 			debug: true,
 			utf8: true,
-			watch: false,
-			brain: url
+			watch: false
 		};
 		this.bot = new RiveScript({
 			debug: this.opts.debug,
 			utf8: this.opts.utf8,
-			concat: "newline",
+			concat: "newline"
 		});
-		(this.bot as any).ready = false;
-		console.log('loading bot');
-		console.log(this.opts.brain);
-		this.bot.loadFile(this.opts.brain, this.loadingDone, this.loadingError);
+		console.log(this.bot);
+	}
+
+	init(){
+		this.getData().then(function(value) {
+			return value
+		}).then(data => {
+			let url = data.toString();
+			this.bot.loadFile(url, this.loadingDone, this.loadingError);
+		});
 	}
 
 	getData() {
-		var url;
-		this.http.get('./assets/brain/german-1.rive')
-			.map(res => {
-				console.log('map function');
-				console.log(res);
-				console.log(res.url);
-				url = res.url;
-				return res.url;
-			})
-			.subscribe(res => {
-				console.log("res here");
-				console.log(res);
-				return res;
-			});
-		// FIXME: already called in the beginning, should be the last step
-		// Work with promises in next commit
-		console.log("FINAL GETDATA RESULT " + url);
-		return url;
+		return new Promise((resolve, reject) => {
+			this.http.get('./assets/brain/german-1.rive')
+			.toPromise()
+			.then(res => { resolve(res.url); });
+		});
 	}
 
 	loadingDone(batchNumber) {
 		console.log('Bot ready!');
 	  	this.bot.sortReplies();
-	  	(this.bot as any).ready = true;
+	  	this.ready = true;
 	}
 
 	loadingError(error, batchNumber) {
